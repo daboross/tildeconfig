ccf2815bd968664b67e9c85e019be1dd7e8b0a1c
David Ross <daboross@daboross.net>
2020-08-30 12:12:21 -0700

    Add Linux OS detection using lsb_release

--- lib/tildeconfig/predefined_installers.rb
+++ lib/tildeconfig/predefined_installers.rb
@@ -5,4 +5,18 @@ def define_predefined_installers
   def_installer :ubuntu do |packages|
     sh "sudo apt install #{packages.join(' ')}"
   end
+
+  # attempt to detect Linux operating systems using lsb_release
+  def_os_detection do
+    result = nil
+    begin
+      IO.popen(['lsb_release', '--id', '--short']) do |lsb_release_io|
+        result = lsb_release_io.read
+      end
+    rescue StandardError
+      nil
+    else
+      result.strip.downcase.intern
+    end
+  end
 end
